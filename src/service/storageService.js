649802f155b27386c24bfca5ef02fcf81b09770c
Blair Garrett <blair.garrett@sky.uk>
2018-07-02 16:43:35 +0100

    Changing to local storage until updating is in place.

--- src/service/storageService.js
+++ src/service/storageService.js
@@ -1,3 +1,20 @@
+// export function set(key, value){
+//     if (value !== "null") {
+//         sessionStorage.setItem(key, JSON.stringify(value));
+//     }
+// };
+
+// export function get(key){
+//     const sessionValue = sessionStorage.getItem(key);
+
+//     if (sessionValue) {
+//         return JSON.parse(sessionValue);
+//     }
+
+//     return sessionValue;
+// };
+
+// @TODO: Using local storage until I've got updating working.
 export function set(key, value){
     if (value !== "null") {
         sessionStorage.setItem(key, JSON.stringify(value));
