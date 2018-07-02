8686bee3612507dcc7605e7a31b853cdb5b2f448
Blair Garrett <blair.garrett@sky.uk>
2018-07-02 16:44:02 +0100

    Local storage until updating

--- src/service/storageService.js
+++ src/service/storageService.js
@@ -17,12 +17,12 @@
 // @TODO: Using local storage until I've got updating working.
 export function set(key, value){
     if (value !== "null") {
-        sessionStorage.setItem(key, JSON.stringify(value));
+        localStorage.setItem(key, JSON.stringify(value));
     }
 };
 
 export function get(key){
-    const sessionValue = sessionStorage.getItem(key);
+    const sessionValue = localStorage.getItem(key);
 
     if (sessionValue) {
         return JSON.parse(sessionValue);
