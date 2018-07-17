8ac4d621442a36cef3fd41c076b05bec4a3b949e
Blair Garrett <blair.garrett@sky.uk>
2018-07-17 22:44:49 +0100

    Added prettier

--- src/components/RaceDetails.js
+++ src/components/RaceDetails.js
@@ -4,27 +4,31 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 const styles = {
-    raceButton: {
-        textDecoration: 'none',
-        zIndex: '1 !important',
-        color: 'white',
-    },
+  raceButton: {
+    textDecoration: 'none',
+    zIndex: '1 !important',
+    color: 'white',
+  },
 };
 
 function RaceDetails(props) {
-    const { classes, race } = props;
+  const { classes, race } = props;
 
-    return (
-        <div key={race.id}>
-            <Typography variant="headline" color='primary'>
-                <Button variant="contained" color='primary'>
-                    <a href={race.resultsUrl} target="_blank" className={classes.raceButton}>
-                        {race.name} - {race.date}
-                    </a>
-                </Button>
-            </Typography>
-        </div>
-    );
+  return (
+    <div key={race.id}>
+      <Typography variant="headline" color="primary">
+        <Button variant="contained" color="primary">
+          <a
+            href={race.resultsUrl}
+            target="_blank"
+            className={classes.raceButton}
+          >
+            {race.name} - {race.date}
+          </a>
+        </Button>
+      </Typography>
+    </div>
+  );
 }
 
 // @TODO: Can parse all races and get info like elevation, location, hyperlink for race.
--- src/service/searchService.js
+++ src/service/searchService.js
@@ -1,31 +1,39 @@
 import axios from 'axios';
 
 export async function search(runnerName) {
-    let races = null;
+  let races = null;
 
-    await axios.get(`${process.env.REACT_APP_API_SERVER}/runner/${runnerName.toLowerCase()}`)
-    .then(function (response) {
-        races = response.data;
-        return races;
+  await axios
+    .get(
+      `${process.env.REACT_APP_API_SERVER}/runner/${runnerName.toLowerCase()}`,
+    )
+    .then(function(response) {
+      races = response.data;
+      return races;
     })
-    .catch(function (error) {
-        console.log(error);
+    .catch(function(error) {
+      console.log(error);
     });
 
-    return races;
-};
+  return races;
+}
 
 export async function partialSearch(partialName) {
-    let runners = null;
+  let runners = null;
 
-    await axios.get(`${process.env.REACT_APP_API_SERVER}/autocomplete/runner/${partialName.toLowerCase()}`)
-    .then(function (response) {
-        runners = response.data;
-        return runners;
+  await axios
+    .get(
+      `${
+        process.env.REACT_APP_API_SERVER
+      }/autocomplete/runner/${partialName.toLowerCase()}`,
+    )
+    .then(function(response) {
+      runners = response.data;
+      return runners;
     })
-    .catch(function (error) {
-        console.log(error);
+    .catch(function(error) {
+      console.log(error);
     });
 
-    return runners;
-};
+  return runners;
+}
